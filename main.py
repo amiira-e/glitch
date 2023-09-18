043e6a36c5eace4e084a05da2395aa63248b903f
amiira <amiiraemambokus@gmail.com>
2023-09-18 10:39:59 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask,jsonify
+app = Flask (__name__)
+
+@app.route("/")
+def hello():
+    return jsonify({"msg:hello deploy from flask"})
+
+if __name__ == "__main__":
+    app.run()
